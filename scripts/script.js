4cc9edb959895ffa955e142613f1afb75534445e
matheo <matheo.brault@etu.unicaen.fr>
2024-10-12 14:36:46 +0200

    notification code postal invalide

--- scripts/script.js
+++ scripts/script.js
@@ -21,35 +21,80 @@ function update() {
 }
 
 /**
+ * créer une notification avec le texte souhaité (supprime la notification actuel si il y en a une)
+ * @param {String} texte 
+ */
+function createNotification(texte) {
+    if(notification != null) {
+        notification.parentNode.removeChild(notification);
+    }
+
+    let top = document.createElement("div");
+    top.classList.add("fixed-top");
+
+    let container = document.createElement("div");
+    container.classList.add("container");
+    container.classList.add("is-max-tablet");
+
+    let notificationB = document.createElement("div");
+    notificationB.classList.add("notification");
+    notificationB.classList.add("is-danger");
+
+    let button = document.createElement("button");
+    button.classList.add("delete");
+    notificationB.innerHTML = texte;
+
+    notificationB.appendChild(button);
+    container.appendChild(notificationB);
+    top.appendChild(container)
+    document.body.appendChild(top);
+
+    notification = top;
+
+    button.addEventListener('click', () => {
+        notification.parentNode.removeChild(notification);
+        notification = null;
+    });
+}
+
+/**
  * insere les communes listé avec le code postal donné dans le select donné
  * @param {String} postal 
  * @param {document.select} select
  */
 function getCommunes(postal, select) {
-    fetch(`https://geo.api.gouv.fr/communes?codePostal=${postal}&fields=nom,code,codePostal`)
-    .then(response => {
-        if (!response.ok) {
-            throw new Error("Erreur lors de la récupération des données");
-        }
-        return response.json(); // Convertit la réponse en JSON
-    }).then(data => {
-        //console.log("Communes récupérées :", data);
-        console.log(`${data.length} communes récupéré avec le code postal ${postal}`);
-        removeOptions(select);
-        data.forEach(commune => {
-            //console.log(`Commune: ${commune.nom}, Code INSEE: ${commune.code}`);
-            let opt = document.createElement('option');
-            opt.value = commune.code;
-            opt.innerHTML = commune.nom;
-            select.appendChild(opt);
+    if (isNaN(postal)) {
+        //si le code postal donnée contient un caractère qui n'est pas un nombre
+        createNotification(`le code postal "<strong>${postal}</strong>" n'est pas valide`);
+    } else {
+        fetch(`https://geo.api.gouv.fr/communes?codePostal=${postal}&fields=nom,code,codePostal`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Erreur lors de la récupération des données");
+            }
+            return response.json(); // Convertit la réponse en JSON
+        }).then(data => {
+            //console.log("Communes récupérées :", data);
+            console.log(`${data.length} communes récupéré avec le code postal ${postal}`);
+            if (data.length > 0) {
+                removeOptions(select);
+                data.forEach(commune => {
+                    //console.log(`Commune: ${commune.nom}, Code INSEE: ${commune.code}`);
+                    let opt = document.createElement('option');
+                    opt.value = commune.code;
+                    opt.innerHTML = commune.nom;
+                    select.appendChild(opt);
+                });
+                document.getElementById("selectCommuneDiv").classList.remove("is-hidden");
+                document.getElementById("labelSelect").classList.remove("is-hidden");
+                document.getElementById("valider").classList.remove("is-hidden");
+            } else {
+                createNotification(`Aucune commune trouvée pour le code postal <strong>${postal}</strong>`);
+            }
+        }).catch(error => {
+            console.error("Une erreur s'est produite :", error);
         });
-        document.getElementById("selectCommuneDiv").classList.remove("is-hidden");
-        document.getElementById("selectCommuneDiv").style.transition = "all 2s";
-        document.getElementById("labelSelect").classList.remove("is-hidden");
-        document.getElementById("valider").classList.remove("is-hidden");
-    }).catch(error => {
-        console.error("Une erreur s'est produite :", error);
-    });
+    }
 }
 
 /**
@@ -80,6 +125,10 @@ function getMeteo(insee) {
     }
 }
 
+/**
+ * change l'affichage de la météo selon le jour choisit
+ * @param {int} jour le nombre de jour depuis aujourd'hui
+ */
 function updateMeteo(jour) {
     document.getElementById("Tmin").textContent = `Température minimale ${meteo[jour].tmin}°C`;
     document.getElementById("Tmax").textContent = `Température maximale ${meteo[jour].tmax}°C`;
@@ -88,6 +137,10 @@ function updateMeteo(jour) {
     updateIcon(meteo[jour].weather);
 }
 
+/**
+ * modifie l'icône en fonction du paramètre weather
+ * @param {int} weather paramètre weather de la météo
+ */
 function updateIcon(weather){
     if(weather <4){
         document.getElementById("Emoji").innerHTML =`<em class="fa-sun fa-solid">`;
@@ -110,6 +163,16 @@ function updateIcon(weather){
 
 document.addEventListener("DOMContentLoaded", function(){
     document.getElementById("cp").value = "";
+
+    /* notification handler */
+    (document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
+        const $notification = $delete.parentNode;
+
+        $delete.addEventListener('click', () => {
+            $notification.parentNode.removeChild($notification);
+            console.log("supprime");
+        });
+    });
 });
 
 document.getElementById("cp").addEventListener("keyup", update);
@@ -118,5 +181,6 @@ document.getElementById("valider").addEventListener("click", () => {
 })
 const APITOKEN = '7e4130a5c51e4c071da97d29828bea60cf0091b53ca00d105a0b79bd54bd803d';
 let lastPostal = "0"; //sécurité anti spam de requêtes
-let lastInsee = "0";
-let meteo;
+let lastInsee = "0"; //sécurité anti spam de requêtes
+let meteo; //sauvegarde des informations de météo
+let notification;
